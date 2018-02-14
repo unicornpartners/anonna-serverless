c36f62b34f6863a34bb532cb26b75671e83748e8
axeman <axeman@amazon.com>
2018-02-14 14:37:21 -0500

    Added Character Selection

--- src/app.js
+++ src/app.js
@@ -22,14 +22,35 @@ function formatResponse(avatar,text){
     return resp;
 }
 
-function loadAvatar() {
+function loadAvatar(index) {
+    console.info('got index ' + index);
     console.info('Loading Avatars');
     let avatars = JSON.parse(fs.readFileSync('./resources/avatars.json'));
     console.info(`Loaded ${avatars.length} avatars`);
-    let avatar = avatars[getRandomInt(0,avatars.length)];
+    let avatar;
+    if (index) {
+        console.info("User selected character");
+    avatar = avatars[index];
+    } else {
+        console.info("No character selected using random");
+    avatar = avatars[getRandomInt(0,avatars.length)];
+    }
     console.info(`Anonna will respond as ${avatar.username}`);
     return avatar;
 }
+
+function listAvatars() {
+    console.info('Listing Avatars');
+    let avatars = JSON.parse(fs.readFileSync('./resources/avatars.json'));
+    console.info(`Loaded ${avatars.length} avatars`);
+    let avatarlist = "character list, select the number next to the character with the !speakas command\n";
+    for (let index in avatars) {
+        avatarlist += index + " : " + avatars[index].username + "\n";
+    }
+    return avatarlist;
+}
+
+
 
 function postToWebhook(response,callback){
     let slack_url = url.parse(process.env['SLACK_WEBHOOK_URL']);
@@ -63,12 +84,48 @@ function postToWebhook(response,callback){
 exports.anonamize = (event,context,callback) => {
     console.info('Receiving Anonamization Request');
 
-    postToWebhook(formatResponse(loadAvatar(),event.text),function(err,data){
-        if(err) {
-            console.error('There has been an error');
-            console.error(new Error(err));
-            callback(JSON.stringify({text: 'Something went wrong, please try again.'}));
-        }
-        callback(null,data);
-    });
+    let txtCommand = event.text.split(" ", 1);
+    
+    console.info("command is " + txtCommand);
+    
+    switch (txtCommand.toString().toLowerCase()) {
+        case "!list":
+            console.info("!list selected")
+            let list = listAvatars();
+            console.info(list);
+            callback(null,JSON.stringify({text: list}));
+            break;
+        case "!speakas":
+            console.info("$speak selected")
+            let aindex = event.text.split(" ", 2)[1];
+            if (Number.isInteger(parseInt(aindex))) {
+                postToWebhook(formatResponse(loadAvatar(aindex),event.text.replace(/!speakas [0-9]+\s/,"")),function(err,data){
+                if(err) {
+                    console.error('There has been an error');
+                    console.error(new Error(err));
+                    callback(JSON.stringify({text: 'Something went wrong, please try again.'}));
+                }
+                callback(null,data);
+                });
+            } else {
+                callback(null,JSON.stringify({text: 'Valid character not selected. Please use !list subcommand and put character number not name in format /anon !speakas <character number> <your message>\n example: /anon !speakas 10 I have an important question'}));
+            }
+            
+            break;
+        default:
+            postToWebhook(formatResponse(loadAvatar(),event.text),function(err,data){
+            if(err) {
+                console.error('There has been an error');
+                console.error(new Error(err));
+                callback(JSON.stringify({text: 'Something went wrong, please try again.'}));
+            }
+            callback(null,data);
+            
+            });
+           
+            break;
+    }
+    
+    
 };
+
